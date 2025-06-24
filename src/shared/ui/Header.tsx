2dc7060c71024d06564db3afdc5d0f205eab1718
bae080311 <s24024@gsm.hs.kr>
2025-06-25 08:49:34 +0900

    feat: header style

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,16 +1,19 @@
+import Link from 'next/link';
 import Bell from '../asset/svg/Bell';
 import { Badge } from '@/components/ui/badge';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between">
-      <h1 className="text-2xl font-bold text-gray-900">관리자 대시보드</h1>
+    <header className="flex items-center bg-white justify-between p-4">
+      <h1 className="text-2xl font-bold text-gray-900">시민화폐광산</h1>
       <div className="flex items-center space-x-4">
-        <Badge variant="outline" className="flex items-center space-x-1">
-          <Bell />
-          <span>4</span>
-        </Badge>
+        <Link href="/bell">
+          <Badge variant="outline" className="flex items-center space-x-1">
+            <Bell />
+            <span>4</span>
+          </Badge>
+        </Link>
       </div>
-    </div>
+    </header>
   );
 }
