0e9d42c0eeb720acb36cd202389b8afc3d98b736
bae080311 <s24024@gsm.hs.kr>
2025-06-25 15:13:40 +0900

    refactor: use useCallBack

--- src/shared/ui/Nav.tsx
+++ src/shared/ui/Nav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Bell from '../asset/svg/Bell';
 import Users from '../asset/svg/Users';
 import FileText from '../asset/svg/FileText';
@@ -18,18 +18,30 @@ export default function Nav() {
     setNav(pathname.slice(1) as Nav);
   }, [pathname]);
 
+  const handleMember = useCallback(()=>{
+    router.push("/member")
+  }, [router])
+
+  const handleNotice = useCallback(()=>{
+    router.push("/Notice")
+  }, [router])
+
+  const handleNotification = useCallback(()=>{
+    router.push("/notification")
+  }, [router])
+
   return (
     <Tabs className='px-4' value={nav} onValueChange={(value) => setNav(value as Nav)}>
       <TabsList className="grid w-full grid-cols-3">
-        <TabsTrigger onClick={() => router.push("/member")} value="member" className="flex items-center space-x-2">
+        <TabsTrigger onClick={handleMember} value="member" className="flex items-center space-x-2">
           <Users />
           <span>회원관리</span>
         </TabsTrigger>
-        <TabsTrigger onClick={() => router.push("/notice")} value="notice" className="flex items-center space-x-2">
+        <TabsTrigger onClick={handleNotice} value="notice" className="flex items-center space-x-2">
           <FileText />
           <span>공지사항</span>
         </TabsTrigger>
-        <TabsTrigger onClick={() => router.push("/notification")} value="notification" className="flex items-center space-x-2">
+        <TabsTrigger onClick={handleNotification} value="notification" className="flex items-center space-x-2">
           <Bell />
           <span>알림</span>
         </TabsTrigger>
