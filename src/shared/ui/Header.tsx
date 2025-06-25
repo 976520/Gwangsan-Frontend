f16044d7beafa241a94a87e2a94e02f1fc7eae07
bae080311 <s24024@gsm.hs.kr>
2025-06-25 09:06:55 +0900

    feat: redirect

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="flex items-center bg-white justify-between p-4">
       <h1 className="text-2xl font-bold text-gray-900">시민 화폐 광산</h1>
       <div className="flex items-center space-x-4">
-        <Link href="/bell">
+        <Link href="/notification">
           <Badge variant="outline" className="flex items-center space-x-1">
             <Bell />
             <span>4</span>
--- src/shared/ui/Nav.tsx
+++ src/shared/ui/Nav.tsx
@@ -5,24 +5,25 @@ import { useState } from 'react';
 import Bell from '../asset/svg/Bell';
 import Users from '../asset/svg/Users';
 import FileText from '../asset/svg/FileText';
+import { useRouter } from 'next/navigation';
 
 type Nav = 'member' | 'notice' | 'bell';
 
 export default function Nav() {
   const [nav, setNav] = useState<Nav>('member');
-
+  const router = useRouter();
   return (
     <Tabs className='px-4' value={nav} onValueChange={(value) => setNav(value as Nav)}>
       <TabsList className="grid w-full grid-cols-3">
-        <TabsTrigger value="member" className="flex items-center space-x-2">
+        <TabsTrigger onClick={()=>router.push("/member")} value="member" className="flex items-center space-x-2">
           <Users />
           <span>회원관리</span>
         </TabsTrigger>
-        <TabsTrigger value="notice" className="flex items-center space-x-2">
+        <TabsTrigger onClick={()=>router.push("/notice")} value="notice" className="flex items-center space-x-2">
           <FileText />
           <span>공지사항</span>
         </TabsTrigger>
-        <TabsTrigger value="bell" className="flex items-center space-x-2">
+        <TabsTrigger onClick={()=>router.push("/notification")} value="bell" className="flex items-center space-x-2">
           <Bell />
           <span>알림</span>
         </TabsTrigger>
