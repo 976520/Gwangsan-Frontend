63b4e5d9a9c2cd713918eecf8bbbd6012b11a558
bom0320 <s24040@gsm.hs.kr>
2025-06-29 19:54:04 +0900

    chore: separate noticeCard view

--- src/app/notice/[id]/page.tsx
+++ src/app/notice/[id]/page.tsx
@@ -1,46 +1,31 @@
-"use client"
+'use client';
 
-import { fetchNoticeById } from "@/shared/api/fetchNoticeById"
-import { useState, useEffect } from "react"
-import type { Notice } from "@/entities/notice/model/types"
-import NoticeDetail from "@/shared/ui/NoticeDetail/page"
-import { useRouter } from "next/navigation"
-import { mockNotices } from "@/shared/mock/notices"
+import { fetchNoticeById } from '@/shared/api/fetchNoticeById';
+import { useState, useEffect, useCallback } from 'react';
+import type { Notice } from '@/entities/notice/model/types';
+import NoticeDetail from '@/shared/ui/NoticeDetail/page';
+import { useRouter } from 'next/navigation';
+import { mockNotices } from '@/shared/mock/notices';
+import NoticeView from '@/views/noticeView/ui';
 
 export default function Page({ params }: { params: Promise<{ id: string }> }) {
-  const router = useRouter()
-  const [notice, setNotice] = useState<Notice | null>(null)
+  const router = useRouter();
+  const [notice, setNotice] = useState<Notice | null>(null);
 
   useEffect(() => {
     const fetchNotice = async () => {
       const { id } = await params;
       // const data = await fetchNoticeById(id);
       // setNotice(data);
-      const mock = mockNotices.find((p) => p.id.toString() == id)
-      mock && setNotice(mock)
-    }
+      const mock = mockNotices.find((p) => p.id.toString() == id);
+      mock && setNotice(mock);
+    };
     fetchNotice();
-  }, [params])
+  }, [params]);
 
-  const onBack = () => {
-    router.push('/notice')
-  }
+  const onBack = useCallback(() => {
+    router.push('/notice');
+  }, [router]);
 
-  return (
-    <div>
-      {
-        notice ? (
-          <NoticeDetail notice={notice} onBack={onBack} />
-        ) : (
-          <div>
-            <p>로딩중...</p>
-          </div>
-        )
-
-
-      }
-    </div>
-
-  )
+  return <NoticeView notice={notice} onBack={onBack} />;
 }
-
