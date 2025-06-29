78d223a6d8ebb5c5b01159efdd85401f7fa966a6
bom0320 <s24040@gsm.hs.kr>
2025-06-29 19:58:27 +0900

    chore: createNoticeForm api exception handling

--- src/shared/api/createNoticeForm.ts
+++ src/shared/api/createNoticeForm.ts
@@ -1,7 +1,11 @@
-import { Notice } from "@/entities/notice/model/types";
-import { instance } from "../lib/axios";
+import { Notice } from '@/entities/notice/model/types';
+import { instance } from '../lib/axios';
 
 export const createNoticeForm = async (notice: Notice) => {
-    // 명세서 보고, 보내는 값 변경
-    const { data } = await instance.post("/api/post", notice);
-}
+  // 명세서 보고, 보내는 값 변경
+  try {
+    await instance.post(`/api/post`, notice);
+  } catch (error) {
+    console.error('공지사항 생성 실패:', error);
+  }
+};
