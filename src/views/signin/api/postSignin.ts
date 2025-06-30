c80d6a8f78c71d605c0fcfc06f9234a067f60351
bae080311 <s24024@gsm.hs.kr>
2025-06-30 23:43:22 +0900

    feat: post signin

---
+++ src/views/signin/api/postSignin.ts
@@ -0,0 +1,19 @@
+import { instance } from '@/shared/lib/axios';
+import { SigninForm } from '@/shared/model/authSchema';
+import { AxiosError } from 'axios';
+import { toast } from 'sonner';
+
+export const postSignin = async (
+  data: SigninForm,
+): Promise<{ success: boolean; error: string }> => {
+  try {
+    await instance.post('/auth/signin', data);
+    toast.success('로그인에 성공했습니다.');
+    return { success: true, error: '' };
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      return { success: false, error: e.message };
+    }
+    return { success: false, error: '알 수 없는 오류가 발생했습니다.' };
+  }
+};
