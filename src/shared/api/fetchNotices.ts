ff588665d0e9d7c84fdd9a210ed31728825f1690
bom0320 <s24040@gsm.hs.kr>
2025-06-30 11:09:11 +0900

    fix: type fetchNotices based on API spec

--- src/shared/api/fetchNotices.ts
+++ src/shared/api/fetchNotices.ts
@@ -1,6 +1,7 @@
-import { instance } from "../lib/axios";
+import { instance } from '../lib/axios';
+import type { Notice } from '@/entities/notice/model/types';
 
-export const fetchNotices = async () => {
-    const { data } = await instance.get("/api/post");
-    return data;
-}
+export const fetchNotices = async (): Promise<Notice[]> => {
+  const { data } = await instance.get('/api/post');
+  return data;
+};
