43c4b96b4c6ec32533179840ec73b70ee50a81c9
bom0320 <s24040@gsm.hs.kr>
2025-06-30 23:57:41 +0900

    refactor: reuse component

--- src/app/notice/[id]/page.tsx
+++ src/app/notice/[id]/page.tsx
@@ -1,7 +1,18 @@
 'use client';
 
 import NoticeDetailCard from '@/features/notice/view/ui/NoticeDetailCard';
+import { useEffect, useState } from 'react';
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <NoticeDetailCard id={params.id} />;
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  const [id, setId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchId = async () => {
+      const { id } = await params;
+      setId(id);
+    };
+    fetchId();
+  }, [params]);
+
+  return <div>{id != null && <NoticeDetailCard id={id} />}</div>;
 }
--- src/shared/ui/FileUpload/index.tsx
+++ src/shared/ui/FileUpload/index.tsx
@@ -1,42 +1,48 @@
-'use client'
+'use client';
 
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Upload } from 'lucide-react'
-import React, { useRef } from 'react'
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Upload } from 'lucide-react';
+import React, { useRef } from 'react';
 
 interface FileUploadProps {
-  id: string
-  onChange: (file: File | null) => void
+  id: string;
+  onChange: (file: File | null) => void;
 }
 
 export default function FileUpload({ id, onChange }: FileUploadProps) {
-  const inputRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0] ?? null
-    onChange(file)
-  }
+    const file = e.target.files?.[0] ?? null;
+    onChange(file);
+  };
 
   const handleButtonClick = () => {
-    inputRef.current?.click()
-  }
+    inputRef.current?.click();
+  };
 
   return (
     <div>
-      <div className="flex items-center space-x-2 mt-1">
+      <div className="mt-1 flex items-center space-x-2">
         <Input
           id={id}
           ref={inputRef}
           type="file"
           accept="image/*"
-          onChange={handleChange}
-        />    
-        <Button variant="outline" size="sm" type="button" onClick={handleButtonClick}>
+          onChange={handleChange} // MIME 타입만 체크하면 파일 내용 검증이 제대로 안될 것 같아요 라고 코멘트떠쒀
+          multiple
+        />
+        <Button
+          variant="outline"
+          size="sm"
+          type="button"
+          onClick={handleButtonClick}
+        >
           <Upload className="h-4 w-4" />
         </Button>
       </div>
     </div>
-  )
+  );
 }
