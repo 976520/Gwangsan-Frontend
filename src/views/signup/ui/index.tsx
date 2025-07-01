b59ab66a61120d811fcdc6871bc4f10ff74827b0
bae080311 <s24024@gsm.hs.kr>
2025-07-01 15:12:37 +0900

    fix: padding

--- src/views/signup/ui/index.tsx
+++ src/views/signup/ui/index.tsx
@@ -8,7 +8,7 @@ import SignupForm from '@/widgets/SignupForm/ui';
 
 export default function SignupView() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screens flex items-center justify-center bg-gray-50 px-4 py-10 sm:px-6 lg:px-8">
       <Card className="w-full max-w-md">
         <CardHeader className="space-y-1">
           <CardTitle className="text-center text-2xl font-bold">
