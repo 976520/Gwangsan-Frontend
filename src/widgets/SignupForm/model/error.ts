7368a53ba7f002b0e1bfde8faa02bac739433ecd
bae080311 <s24024@gsm.hs.kr>
2025-07-01 14:34:26 +0900

    feat: error type

---
+++ src/widgets/SignupForm/model/error.ts
@@ -0,0 +1,12 @@
+interface SignupError {
+  phoneNumber?: string[];
+  password?: string[];
+  verificationCode?: string[];
+  nickname?: string[];
+  placeId?: string[];
+  dongId?: string[];
+  specialties?: string[];
+  name?: string[];
+  recommender?: string[];
+  confirmPassword?: string[];
+}
