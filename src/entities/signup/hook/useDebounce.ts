9ac676590e1f357f75f65b239e9ba0cee37726a8
bae080311 <s24024@gsm.hs.kr>
2025-07-01 09:56:21 +0900

    feat: useDebounce hook

---
+++ src/entities/signup/hook/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounce<T>(value: T): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, [value, 200]);
+
+  return debouncedValue;
+}
