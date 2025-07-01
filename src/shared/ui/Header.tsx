0ae1345aa0877d6c0b27819cdacd2152a5e0183e
bae080311 <s24024@gsm.hs.kr>
2025-07-01 15:04:49 +0900

    feat: hidden header, nav

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,19 +1,26 @@
+'use client';
+
 import Link from 'next/link';
 import Bell from '../asset/svg/Bell';
 import { Badge } from '@/components/ui/badge';
+import { usePathname } from 'next/navigation';
+import { authConfig } from '../config/auth';
 
 export default function Header() {
-  return (
-    <header className="flex items-center bg-white justify-between p-4">
-      <h1 className="text-2xl font-bold text-gray-900">시민 화폐 광산</h1>
-      <div className="flex items-center space-x-4">
-        <Link href="/notification">
-          <Badge variant="outline" className="flex items-center space-x-1">
-            <Bell />
-            <span>4</span>
-          </Badge>
-        </Link>
-      </div>
-    </header>
-  );
+  const pathname = usePathname();
+  if (!authConfig.publicPages.includes(pathname)) {
+    return (
+      <header className="flex items-center justify-between bg-white p-4">
+        <h1 className="text-2xl font-bold text-gray-900">시민 화폐 광산</h1>
+        <div className="flex items-center space-x-4">
+          <Link href="/notification">
+            <Badge variant="outline" className="flex items-center space-x-1">
+              <Bell />
+              <span>4</span>
+            </Badge>
+          </Link>
+        </div>
+      </header>
+    );
+  }
 }
--- src/shared/ui/Nav.tsx
+++ src/shared/ui/Nav.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useCallback, useMemo } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import { Bell, FileText, Users } from '../asset/svg';
+import { authConfig } from '../config/auth';
 
 type Nav = 'member' | 'notice' | 'notification';
 
@@ -18,26 +19,34 @@ export default function Nav() {
     return 'member';
   }, [pathname]);
 
-  const handleChange = useCallback((value: string)=>{
-    router.push("/" + value)
-  }, [router])
-
-  return (
-    <Tabs className='px-4' value={currentTab} onValueChange={handleChange}>
-      <TabsList className="grid w-full grid-cols-3">
-        <TabsTrigger value="member" className="flex items-center space-x-2">
-          <Users />
-          <span>회원관리</span>
-        </TabsTrigger>
-        <TabsTrigger  value="notice" className="flex items-center space-x-2">
-          <FileText />
-          <span>공지사항</span>
-        </TabsTrigger>
-        <TabsTrigger value="notification" className="flex items-center space-x-2">
-          <Bell />
-          <span>알림</span>
-        </TabsTrigger>
-      </TabsList>
-    </Tabs>
+  const handleChange = useCallback(
+    (value: string) => {
+      router.push('/' + value);
+    },
+    [router],
   );
+
+  if (!authConfig.publicPages.includes(pathname)) {
+    return (
+      <Tabs className="px-4" value={currentTab} onValueChange={handleChange}>
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="member" className="flex items-center space-x-2">
+            <Users />
+            <span>회원관리</span>
+          </TabsTrigger>
+          <TabsTrigger value="notice" className="flex items-center space-x-2">
+            <FileText />
+            <span>공지사항</span>
+          </TabsTrigger>
+          <TabsTrigger
+            value="notification"
+            className="flex items-center space-x-2"
+          >
+            <Bell />
+            <span>알림</span>
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
+    );
+  }
 }
