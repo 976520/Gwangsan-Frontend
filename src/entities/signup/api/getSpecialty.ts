eaa403ca85b64240325aca7a03920944acf8b85f
bae080311 <s24024@gsm.hs.kr>
2025-07-03 04:27:58 +0900

    fix: request type

--- src/entities/signup/api/getSpecialty.ts
+++ src/entities/signup/api/getSpecialty.ts
@@ -1,12 +1,12 @@
 import { instance } from '@/shared/lib/axios';
 import { toast } from 'sonner';
+import { Specialty } from '../model/useGetSpecialty';
 
-export const getSpecialty = async (): Promise<string[]> => {
+export const getSpecialty = async (): Promise<Specialty[]> => {
   try {
-    const res = await instance.get('/related-keyword');
-    return res.data.length === 0 ? [] : res.data;
+    return (await instance.get('/related-keyword')).data;
   } catch (error) {
     toast.error('특기를 불러오는데 실패하였습니다');
-    throw error;
+    return [];
   }
 };
