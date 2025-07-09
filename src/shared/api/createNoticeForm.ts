1a955f63a61b1976c29c9ec890b84fb52a581792
bom0320 <s24040@gsm.hs.kr>
2025-07-09 13:56:01 +0900

    refactor: remove toast

--- src/shared/api/createNoticeForm.ts
+++ src/shared/api/createNoticeForm.ts
@@ -1,19 +1,14 @@
 import { FormValues } from '@/features/notice/model/NoticeForm';
 import { instance } from '../lib/axios';
-import { toast } from 'sonner';
 
 export const createNoticeForm = async (
   data: FormValues,
   imageIds: number[],
 ) => {
-  try {
-    await instance.post(`/post`, {
-      title: data.title,
-      content: data.content,
-      placeName: data.role,
-      imageIds: imageIds,
-    });
-  } catch (error) {
-    toast.error(`${error}`);
-  }
+  await instance.post(`/post`, {
+    title: data.title,
+    content: data.content,
+    placeName: data.role,
+    imageIds: imageIds,
+  });
 };
