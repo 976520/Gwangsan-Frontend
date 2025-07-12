001ba0529c98469c744585d2fb54a8c7dcf7d331
bae080311 <s24024@gsm.hs.kr>
2025-07-12 15:08:00 +0900

    fix: phoneNumber schema

--- src/shared/model/phoneNumberSchema.ts
+++ src/shared/model/phoneNumberSchema.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const phoneNumberSchema = z
   .string()
   .trim()
+  .transform((val) => val.replace(/[^\d-]/g, ''))
   .refine((val) => /^01[016789]-?\d{3,4}-?\d{4}$/.test(val), {
     message: '유효한 휴대폰 번호 형식이 아닙니다.',
   });
