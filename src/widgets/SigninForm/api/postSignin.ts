35123a6cde74b5ca5889dc7cba01f99b512c9c95
bae080311 <s24024@gsm.hs.kr>
2025-07-13 16:20:35 +0900

    feat: handle error

--- src/widgets/SigninForm/api/postSignin.ts
+++ src/widgets/SigninForm/api/postSignin.ts
@@ -2,6 +2,7 @@ import { instance } from '@/shared/lib/axios';
 import { SigninForm } from '@/shared/model/authSchema';
 import { AxiosError } from 'axios';
 import { setCookie } from 'cookies-next';
+import { toast } from 'sonner';
 
 export const postSignin = async (
   data: SigninForm,
@@ -12,11 +13,14 @@ export const postSignin = async (
     setCookie('refreshToken', res.data.refreshToken, {
       maxAge: 60 * 60 * 24 * 7,
     });
+    toast.success('로그인에 성공했습니다.');
     return { success: true, error: '' };
   } catch (e) {
     if (e instanceof AxiosError) {
+      toast.error(e.response?.data.message || '로그인에 실패했습니다.');
       return { success: false, error: e.message };
     }
+    toast.error('알 수 없는 오류가 발생했습니다.');
     return { success: false, error: '알 수 없는 오류가 발생했습니다.' };
   }
 };
